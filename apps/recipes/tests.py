3f37b13981eb1824f8712e0a664a8750f102e926
Jeremy DeLong <chefjeremy@delongaz.com>
2022-11-20 15:00:17 +0000

    using mock to create objects

--- apps/recipes/tests.py
+++ apps/recipes/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
 from apps.recipes.models import Recipes
+from apps.recipes.forms import recipesform
+from apps.login.models import User
 
 # Create your tests here.
-
+import mock
 
 class RecipeTesting(TestCase):
 
@@ -44,6 +46,32 @@ class RecipeTesting(TestCase):
 
     def test_recipe_skillLevel(self):
         self.assertEqual(str(self.recipe.skillLevel), '3')
+
+    def test_recipe_save(self):
+
+        testuser = mock.Mock(spec=User)
+        testuser.username = 'testy'
+        testuser.email = 'testy@mctesterston.com'
+        testuser.password = 'Pass123'
+        testrecipe = mock.Mock(spec=Recipes)
+        testrecipe.name = self.recipe.name
+        self.assertEqual(str(testrecipe.name), str(self.recipe.name))
+        testrecipe.author = testuser
+        testrecipe.cookHour = self.recipe.cookHour
+        self.assertEqual(str(testrecipe.cookHour), str(self.recipe.cookHour))
+        testrecipe.cookMin = self.recipe.cookMin
+        self.assertEqual(str(testrecipe.cookMin), str(self.recipe.cookMin))
+        testrecipe.description = self.recipe.description
+        self.assertEqual(str(testrecipe.description), str(self.recipe.description))
+        testrecipe.servingQuantity = self.recipe.servingQuantity
+        self.assertEqual(str(testrecipe.servingQuantity), str(self.recipe.servingQuantity))
+        testrecipe.prepHour = self.recipe.prepHour
+        self.assertEqual(str(testrecipe.prepHour), str(self.recipe.prepHour))
+        testrecipe.prepMin = self.recipe.prepMin
+        self.assertEqual(str(testrecipe.prepMin), str(self.recipe.prepMin))
+        testrecipe.skillLevel = self.recipe.skillLevel
+        self.assertEqual(str(testrecipe.skillLevel), str(self.recipe.skillLevel))
+         
 
     class Meta:
         app_label = 'recipes'
